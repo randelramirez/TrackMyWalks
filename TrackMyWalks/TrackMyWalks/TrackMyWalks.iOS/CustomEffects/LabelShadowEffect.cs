508824cebf76c32fa6fab2ea22600e978b1eb601
Randel Ramirez <randelramirez1@gmail.com>
2020-02-21 18:16:01 +0800

    Added effects for label and button, added global styles and implcit styles

---
+++ TrackMyWalks/TrackMyWalks/TrackMyWalks.iOS/CustomEffects/LabelShadowEffect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportEffect(typeof(TrackMyWalks.iOS.CustomEffects.LabelShadowEffect), "LabelShadowEffect")]
+namespace TrackMyWalks.iOS.CustomEffects
+{
+    public class LabelShadowEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            try
+            {
+                Control.Layer.CornerRadius = 5;
+                Control.Layer.ShadowColor = Color.Black.ToCGColor();
+                Control.Layer.ShadowOffset = new CGSize(4, 4);
+                Control.Layer.ShadowOpacity = 0.5f;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: " + ex.Message);
+            }
+        }
+        protected override void OnDetached()
+        {
+        }
+    }
+}
---
+++ TrackMyWalks/TrackMyWalks/TrackMyWalks/CustomEffects/ButtonShadowEffect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TrackMyWalks.CustomEffects
+{
+    public class ButtonShadowEffect : RoutingEffect
+    {
+        public ButtonShadowEffect() : base("MyCompanyName.ButtonShadowEffect")
+        {
+        }
+    }
+}
